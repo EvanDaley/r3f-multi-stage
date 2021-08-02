dbc7c00f358033efaa7f0e89d058e35b927d6c47
Evan Daley <daley.evan@gmail.com>
2021-08-02 11:38:28 -0700

    Fixing an HDRI loading issue

--- src/components/three_components/scenes/Scene1.js
+++ src/components/three_components/scenes/Scene1.js
@@ -4,7 +4,7 @@ import WobblyTorus from '../objects/WobblyTorus'
 import OxygenContainer2 from '../objects/OxygenContainer2'
 import OxygenContainer3 from '../objects/OxygenContainer3'
 import AbstractSphere from '../objects/AbstractSphere'
-import { OrbitControls, Stats, Stage, Loader, PerspectiveCamera, Environment } from '@react-three/drei';
+import { OrbitControls, Stats, Stage, Loader, PerspectiveCamera, Environment, useTexture } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React, { useState, useEffect, Suspense } from 'react';
 
@@ -16,16 +16,16 @@ export default function Scene({ sceneIndex }) {
     options: [
       'city',
       'sunset',
-      'dawn',
-      'night',
-      'warehouse',
+      // 'dawn',
+      // 'night',
+      // 'warehouse',
       'forest',
-      'apartment',
-      'studio',
-      'park',
-      'lobby',
+      // 'apartment',
+      // 'studio',
+      // 'park',
+      // 'lobby',
     ],
-    value: 'city',
+    value: 'forest',
   }
 
   const { hdri } = useControls('environment/lighting', { hdri: environmentControls })
@@ -35,27 +35,12 @@ export default function Scene({ sceneIndex }) {
   return (
     <>
       <Stage adjustCamera={false} contactShadow={true} shadows={true}>
-        {/* <Suspense fallback={null}> */}
-          {/* <Environment preset={'forest'} background={true} /> */}
-        {/* </Suspense> */}
-        <Environment preset={hdri} background={false} />
+        <Suspense fallback={null}>
+          <Environment preset={hdri} background={false} />
+        </Suspense>
         <OxygenContainer3 rotation={[0, 90, 0]} position={[6, 0, 0]} />
         <WobblyTorus animationOffset={[6, 2, 0]} />
-
-        {/* <mesh
-          castShadow
-          receiveShadow
-          position={[1, 2, 0]}
-        >
-          <torusKnotBufferGeometry args={[.5, 0.25, 128, 32]} />
-          <meshPhysicalMaterial roughness={0} metalness={1} />
-        </mesh> */}
-        {/* {hdri ? <Environment preset={hdri} background={false} /> : null} */}
-
-        {/* <AbstractSphere animationOffset={[6, 2, 0]} color="red" /> */}
-        {/* <AbstractSphere scale={[0.7, 0.7, 0.7]} animationOffset={[6, 2, 0]} color="red" /> */}
       </Stage>
     </>
   );
 }
-
