c7ca1fd92e84d6cbff2faadd72a04270e4a599e1
Evan Daley <daley.evan@gmail.com>
2021-08-03 11:51:53 -0700

    Cleaning up scene 2. Prepping to add the image plane.

--- src/components/three_components/scenes/Scene2.js
+++ src/components/three_components/scenes/Scene2.js
@@ -1,40 +1,16 @@
-import LinesRobot from '../objects/LinesRobot'
-import OxygenContainer from '../objects/OxygenContainer'
 import OxygenContainer2 from '../objects/OxygenContainer2'
-import OxygenContainer3 from '../objects/OxygenContainer3'
 import AbstractSphere from '../objects/AbstractSphere'
 import { OrbitControls, Stats, Stage, Loader, PerspectiveCamera, Environment } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React, { useState, useEffect, Suspense } from 'react';
 
 export default function Scene({ sceneIndex }) {
-  const environmentOptions = [
-    'warehouse',
-    'sunset',
-    'dawn',
-    'night',
-    'forest',
-    'apartment',
-    'studio',
-    'city',
-    'park',
-    'lobby',
-  ]
-
   return (
     <>
-      {/* <color attach="background" args={"black"} /> */}
-
       <Stage adjustCamera={false} contactShadow={true} shadows={true} >
-        {/* <Environment preset={environmentOptions[4]} background={false}/> */}
-
         <OxygenContainer2 rotation={[0, 80, 0]} />
         <AbstractSphere animationOffset={[0, 2, 0]} color="#bbbbbb" />
         <AbstractSphere scale={[0.7, 0.7, 0.7]} animationOffset={[0, 2, 0]} color="#bbbbbb" />
-
-        {/* <OxygenContainer3 rotation={[0, 90, 0]} position={[6, 0, 0]} />
-        <AbstractSphere animationOffset={[6, 2, 0]} color="red" />
-        <AbstractSphere scale={[0.7, 0.7, 0.7]} animationOffset={[6, 2, 0]} color="red" /> */}
       </Stage>
     </>
   );
